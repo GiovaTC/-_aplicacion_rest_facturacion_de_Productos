c93f86a20f71a4322b3ba75b6c6d4e8eef9b1737
gtapiero <giovanny.tapiero@pi.edu.co>
2026-05-17 13:37:09 -0500

    commit - ApplicationDbContext .

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,6 +1,26 @@
-﻿namespace FacturacionAPI.Data
+﻿using FacturacionAPI.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace FacturacionAPI.Data
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext : DbContext
     {
-    }
-}
+        public ApplicationDbContext(
+            DbContextOptions<ApplicationDbContext> options) 
+            : base(options)
+        {
+        }
+        
+        public DbSet<Factura> Facturas { get; set; }
+        public DbSet<FacturaDetalle> FacturaDetalle { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Factura>()
+                .ToTable("FACTURAS");
+
+            modelBuilder.Entity<FacturaDetalle>()
+                .ToTable("FACTURA_DETALLE");
+        }
+    }   
+}   
