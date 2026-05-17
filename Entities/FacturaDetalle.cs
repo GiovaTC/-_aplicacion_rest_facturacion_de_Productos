0b8dd7d6b1f61976ad0cffeef0398b4fdb9523a7
gtapiero <giovanny.tapiero@pi.edu.co>
2026-05-17 13:30:20 -0500

    commit - FacturaDetalle / .

--- Entities/FacturaDetalle.cs
+++ Entities/FacturaDetalle.cs
@@ -1,6 +1,19 @@
-﻿namespace FacturacionAPI.Entities
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FacturacionAPI.Entities
 {
     public class FacturaDetalle
     {
-    }
+        [Key]
+        public int Id_Detalle { get; set; }
+        public string Producto { get; set; }
+        public int Cantidad { get; set; }
+        public decimal Precio { get; set; }
+        public decimal Subtotal { get; set; }
+        
+        [ForeignKey("Factura")]
+        public int Id_Factura { get; set; }
+        public Factura Factura { get; set; }
+    }   
 }
