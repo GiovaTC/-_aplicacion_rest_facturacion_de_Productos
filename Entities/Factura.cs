e91d41703032f7d7b2abc3380d4cbeb338b79ae0
gtapiero <giovanny.tapiero@pi.edu.co>
2026-05-17 13:26:09 -0500

    commit - Factura .

--- Entities/Factura.cs
+++ Entities/Factura.cs
@@ -1,6 +1,14 @@
-﻿namespace FacturacionAPI.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FacturacionAPI.Entities
 {
     public class Factura
     {
+        [Key]
+        public int Id_Factura { get; set; }
+        public string Cliente { get; set; }
+        public DateTime Fecha_Factura { get; set; }
+        public decimal Total { get; set; }
+        public List<FacturaDetalle> Detalles { get; set; }
     }
-}
+}   
