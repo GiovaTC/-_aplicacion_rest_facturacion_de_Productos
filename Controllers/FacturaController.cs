6c928223051d042aca1824a1081233c2fe2b6991
gtapiero <giovanny.tapiero@pi.edu.co>
2026-05-17 13:46:30 -0500

    commit - FacturaController .

--- Controllers/FacturaController.cs
+++ Controllers/FacturaController.cs
@@ -1,6 +1,31 @@
-﻿namespace FacturacionAPI.Controllers
+﻿using FacturacionAPI.Entities;
+using FacturacionAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace FacturacionAPI.Controllers
 {
-    public class FacturaController
+    [ApiController]
+    [Route("api/[controller]")]
+    public class FacturaController : ControllerBase
     {
-    }
+        private readonly FacturaService _service;
+        public FacturaController(FacturaService service)
+        {
+            _service = service;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RegistrarFactura(
+            [FromBody] Factura factura)
+        {
+            await _service.GuardarFactura(factura);
+
+            return Ok(new
+            {
+                mensaje = "Factura registrada correctamente"
+            });
+        }
+    }   
 }
