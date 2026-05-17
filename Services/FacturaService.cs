ffe79205da66984a628ed6bf3e40b1f99206e00a
gtapiero <giovanny.tapiero@pi.edu.co>
2026-05-17 13:43:17 -0500

    commit - FacturaService .

--- Services/FacturaService.cs
+++ Services/FacturaService.cs
@@ -1,6 +1,32 @@
-﻿namespace FacturacionAPI.Services
+﻿using FacturacionAPI.Data;
+using FacturacionAPI.Entities;
+
+namespace FacturacionAPI.Services
 {
     public class FacturaService
     {
+        private readonly ApplicationDbContext _context;
+
+        public FacturaService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task GuardarFactura(Factura factura)
+        {
+            decimal total = 0;
+
+            foreach (var item in factura.Detalles)
+            {
+                item.Subtotal = item.Cantidad * item.Precio;
+                total += item.Subtotal;
+
+            }
+
+            factura.Total = total;
+
+            _context.Facturas.Add(factura);
+            await _context.SaveChangesAsync();
+        }
     }
-}
+}   
